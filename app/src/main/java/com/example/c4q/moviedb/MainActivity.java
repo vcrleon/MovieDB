5541919a500ede3de43e16327b7ec602409a4747
Jervon Arnold <jervonjarnold@gmail.com>
2018-01-30 19:32:39 -0500

    hello

--- app/src/main/java/com/example/c4q/moviedb/MainActivity.java
+++ app/src/main/java/com/example/c4q/moviedb/MainActivity.java
@@ -23,28 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDrawer =  findViewById(R.id.drawerlayout);
-        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
-        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
-            @Override
-            public void onDrawerStateChange(int oldState, int newState) {
-                if (newState == ElasticDrawer.STATE_CLOSED) {
-                    Log.i("MainActivity", "Drawer STATE_CLOSED");
-                }
-            }
 
-            @Override
-            public void onDrawerSlide(float openRatio, int offsetPixels) {
-                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
-            }
-        });
-        mDrawer.toggleMenu();
-//
-//        FragmentManager fm = getSupportFragmentManager();
-//        MenuListFragment mMenuFragment = (MenuListFragment) fm.findFragmentById(R.id.id_container_menu);
-//        if (mMenuFragment == null) {
-//            mMenuFragment = new MenuListFragment();
-//            fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment).commit();
-//        }
     }
 }
