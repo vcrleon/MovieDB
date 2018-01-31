89ec1b1b1a110eeb568cebf38f19cb238793f5f4
Jervon Arnold <jervonjarnold@gmail.com>
2018-01-30 19:25:29 -0500

    added nav

--- app/src/main/java/com/example/c4q/moviedb/MainActivity.java
+++ app/src/main/java/com/example/c4q/moviedb/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.c4q.moviedb;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FlowingDrawer mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawer =  findViewById(R.id.drawerlayout);
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
+        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+            @Override
+            public void onDrawerStateChange(int oldState, int newState) {
+                if (newState == ElasticDrawer.STATE_CLOSED) {
+                    Log.i("MainActivity", "Drawer STATE_CLOSED");
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(float openRatio, int offsetPixels) {
+                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
+            }
+        });
+        mDrawer.toggleMenu();
+//
+//        FragmentManager fm = getSupportFragmentManager();
+//        MenuListFragment mMenuFragment = (MenuListFragment) fm.findFragmentById(R.id.id_container_menu);
+//        if (mMenuFragment == null) {
+//            mMenuFragment = new MenuListFragment();
+//            fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment).commit();
+//        }
     }
 }
